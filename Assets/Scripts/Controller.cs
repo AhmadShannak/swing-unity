4ff3875a6cf6afc34defda8f438bd56f2418f38d
AhmadShannak <shannak.ahmad96@gmail.com>
2019-10-25 14:10:59 +0200

    show direction arrow upon freezing time

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -6,13 +6,30 @@ namespace Stick {
   public class Controller : MonoBehaviour {
     [SerializeField]
     Player player;
+    [SerializeField]
+    GameObject pointer;
+
+    Vector3 mousePos;
 
     void Update() {
       if (Input.GetMouseButton(0)) {
         player.UseFuel();
+        SetPointer();
       } else {
         player.ChargeFuel();
+        ResetPointer();
       }
+    }
+
+    void SetPointer() {
+      pointer.SetActive(true);
+      mousePos = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      pointer.transform.right = new Vector2(mousePos.x, mousePos.y);
+    }
+
+    void ResetPointer() {
+      pointer.SetActive(false);
+      pointer.transform.up = new Vector3(0, 0, 0);
     }
   }
 }
